27f2dce0e102dac51c56981f8f15823707dd3466
Tsybulin.Y <Tsybulin.Y@advabet.com>
2025-04-27 17:35:47 +0300

    feat(): meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'Eugene fun',
+  
+      htmlAttrs: { lang: 'en' },
+  
+      link: [
+        { href: '/favicon.ico' },
+      ]
+    }
+  }
 })
